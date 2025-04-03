0549a3b13e6a4e376892796a3cd541179da14c95
Zeljko Calic <z.calic@levi9.com>
2025-04-03 11:53:57 +0200

    B1 Reload

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,6 +7,14 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
+test('reload', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await page.reload();
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
